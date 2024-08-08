dc5e713749228346b03e2a4dff8cef8721c63463
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2024-08-08 16:40:56 -0400

    properly handle the rotation of arrows as they fall

--- scenes/main.gd
+++ scenes/main.gd
@@ -5,7 +5,11 @@ var arrow_base = preload("res://weapons/arrow.tscn")
 func _on_player_spawn_projectile(location: Marker3D) -> void:
 	var arrow = arrow_base.instantiate()
 	add_child(arrow)
-	arrow.scale = Vector3(40, 40, 40)
 	arrow.position = location.global_position 
+	
+	# rotate arrow based on camera position
 	arrow.rotation = location.get_parent().global_rotation + Vector3(0.0, 3.141, 3.141)
 	arrow.rotation.x = -arrow.rotation.x
+	
+	arrow.init()
+	
--- weapons/arrow.gd
+++ weapons/arrow.gd
@@ -2,6 +2,36 @@ extends Area3D
 
 @export var speed = 100
 
+var y_velocity = 0
+
+var has_collided: bool = false
+var velocity: Vector3 = Vector3.ZERO
+
+var prev_pose: Vector2 = Vector2.ZERO
+
+func init() -> void:
+	velocity.y = -sin(rotation.x) * speed
+	velocity.x = sin(rotation.y) * cos(rotation.x) * speed
+	velocity.z = cos(rotation.y) * cos(rotation.x) * speed
+	
+	prev_pose = Vector2(global_position.x, global_position.y)
+
 func _physics_process(delta: float) -> void:
-	position.x += sin(rotation.y) * speed * delta
-	position.z += cos(rotation.y) * speed * delta
+	# only move if not collided with a wall
+	if !has_collided:
+		# calculate new position
+		position.x += velocity.x * delta
+		position.z += velocity.z * delta
+		position.y += velocity.y * delta
+	
+		velocity.y -= gravity * delta * 4
+		
+		# Calculate angle based on pose delta
+		var pose_delta = Vector2(global_position.x, global_position.y) - prev_pose
+		rotation.x = -pose_delta.angle()
+		prev_pose = Vector2(global_position.x, global_position.y)
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if !body.is_in_group("enemy") and !body.is_in_group("player"):
+		has_collided = true
